498bb686d0d6e317b17ef0843fe7d3dea8445f6c
ck-pc-vb <carel@evalgeniuses.com>
2020-04-01 16:08:13 +0200

    Added initial components, default route, implemented theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogsComponent } from './blogs/blogs.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'blog', component: BlogsComponent },
+  { path: `blog/{article-name}`, component: BlogPostComponent },
+  { path: '',
+    redirectTo: '/blog',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Angular Material Components
+import { MatButtonModule } from '@angular/material/button';
+
+// User Generated Components
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BlogPostComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // MatMenuModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
