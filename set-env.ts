1c21c3b2448b120bd1fecbf44a95f2afecd5db18
ck-pc-vb <carel@evalgeniuses.com>
2020-04-06 17:26:30 +0200

    reworked file so it generates env variables for prod also

--- set-env.ts
+++ set-env.ts
@@ -7,27 +7,31 @@ import { writeFile } from 'fs';
 require('dotenv').config();
 
 const targetPath = './src/environments/environment.ts';
+const targetPath1 = './src/environments/environment.prod.ts';
 const colors = require('colors');
 
 // `environment.ts` file structure
 const envConfigFile = `export const environment = {
-  apiBaseUrl: '${process.env.API_BASE_URL}',
-  apiUrl:     '${process.env.API_URL}',
-  nodeEnv:    '${process.env.NODE_ENV}',
-  production: '${process.env.PRODUCTION}',
   squidex_url: '${process.env.SQUIDEX_URL}',
   client_id:  '${process.env.CLIENT_ID}',
   client_secret: '${process.env.CLIENT_SECRET}',
 };`;
 
-console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+console.log(colors.magenta('The file `environment.ts` & `environment.prod.ts` will be written with the following content: \n'));
 console.log(colors.grey(envConfigFile));
 writeFile(targetPath, envConfigFile, function (err) {
-   if (err) {
-       throw console.error(err);
-   } else {
-       console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
-   }
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+  }
+});
+writeFile(targetPath1, envConfigFile, function (err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(colors.magenta(`Angular environment.prod.ts file generated correctly at ${targetPath1} \n`));
+  }
 });
 
 
